b0b9fa90a8f50fe1394b8b47c10251d34d3744f9
Pradwin <pradwinmadhav6931@gmail.com>
2023-09-06 15:11:41 +0530

    javascript

---
+++ test.js
@@ -0,0 +1,44 @@
+var a;
+var state=true;
+// test(17);
+// const test=(a)=>{
+//     if(a<=15){
+//         state=true;
+//        }
+//        else{
+//            state=false;
+//        }
+//        if(state){
+//            console.log("Is innocent");
+//        }
+//        else{
+//            console.log("Is not innocent");
+//        }
+// }
+// function display(name){
+//     console.log("Hello",name); 
+// }
+// display("Pradwin")
+
+// let display=(name)=>{
+//     console.log(`Hello ${name} how are you`)
+// }
+// display("Pradwin");
+
+
+// let m=(name,Solution="Hello")=>{
+// return `${Solution} ${name}`;
+// }
+// console.log(m("Pradwin"));
+
+let m=(name,Solution="Hello")=>{
+console.log(`${Solution} ${name}`);
+}
+let display=(name)=>{
+    console.log(`Hello ${name} how are you`)
+}
+function all(name){
+    display(name);
+    m(name);
+}
+all("Pradwin");
