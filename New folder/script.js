c02602875d2ef1e80aed86d74f5b36333ec87c12
Pradwin <pradwinsherigar6931@gmail.com>
2023-09-21 19:42:53 +0530

    testing

---
+++ New folder/script.js
@@ -0,0 +1,6 @@
+const fruits = ['banana', 'orange', 'mango', 'lemon'];
+// console.log(fruits.orange? "Found": "not Found");
+indexNo = fruits.indexOf('orange');
+if(indexNo != -1){
+    console.log("Found");
+}
